e40619cc62d142ec0d7461b7c246cc8db3138411
Bruno Braido <Bruno.Braido@dsm.com>
2025-05-30 15:38:35 +0200

    feat(personagem): Capturing the keyboard key arrows

--- snake.py
+++ snake.py
@@ -3,16 +3,22 @@ import time
 
 
 def game_loop(window):
-    window.addstr(f'Aperte alguma tecla para iniciar o jogo...\n')
+    window.addstr(f'Aperte alguma tecla para iniciar o jogo...')
     while True:
         window.timeout(1000)  # Tempo de espera em milissegundos
         char = window.getch()
         window.clear()
-        if char == -1:
-            window.addstr(f'Nenhuma tecla pressionada. Pressione uma tecla para continuar...\n')
-        else:
-            window.addstr(f'Você pressionou: {char}\n')
-
+        match char:
+            case curses.KEY_UP:
+                window.addstr('Mover para cima')
+            case curses.KEY_DOWN:
+                window.addstr('Mover para baixo')
+            case curses.KEY_LEFT:
+                window.addstr('Mover para esquerda')
+            case curses.KEY_RIGHT:
+                window.addstr('Mover para direita')
+            case _:
+                window.addstr(f'Não mover')
 
 if __name__ == "__main__":
     curses.wrapper(game_loop)
