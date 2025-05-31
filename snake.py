cd9521c630233fda7ccb6a505a25d94f3bd83438
Bruno Braido <Bruno.Braido@dsm.com>
2025-05-31 10:45:07 +0200

    feat(border): Added borders checkers

--- snake.py
+++ snake.py
@@ -5,6 +5,8 @@ import time
 def game_loop(window):
     curses.curs_set(0) #hidden cursor
     window.border(0) #no border
+    height, width = window.getmaxyx() #get height and width of the window
+
     personagem = [10, 15]
     window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
     while True:
@@ -23,6 +25,10 @@ def game_loop(window):
                 personagem[1] += 1
             case _: # Not key pressed or not a valid key
                 pass
+        if (personagem[0] <= 0 or personagem[0] >= height - 1):
+            return
+        if (personagem[1] <= 0 or personagem[0] >= width - 1):
+            return
         window.addch(personagem[0], personagem[1], curses.ACS_DIAMOND)
 
 if __name__ == "__main__":
